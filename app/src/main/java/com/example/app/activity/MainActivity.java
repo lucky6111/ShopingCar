24d7b6fbcd9ae6eeae9d432abd67c48b53163a66
lucky6111 <hsueh.hey2@gmail.com>
2020-05-02 20:07:01 +0800

    test_001

---
+++ app/src/main/java/com/example/app/activity/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.app.activity;
+
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.FragmentTabHost;
+
+import com.example.app.R;
+import com.example.app.bean.MessageEvent;
+import com.example.app.bean.Tab;
+import com.example.app.fragment.CategoryFragment;
+import com.example.app.fragment.HomeFragment;
+import com.example.app.fragment.HotFragment;
+import com.example.app.fragment.MineFragment;
+import com.example.app.fragment.ShopCartFragment;
+import com.example.app.utils.ToastUtils;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
+
+    private List<Tab> mTabs = new ArrayList<>();
+    private LayoutInflater  mInflater;
+    private FragmentTabHost mTabhost;
+
+    @Override
+    protected void init() {
+        initTab();
+    }
+
+    @Override
+    protected int getContentResourseId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe (threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {
+        if (event.getType() == 0) {
+            mTabhost.setCurrentTab(1);
+        }
+    }
+
+    private void initTab() {
+        Tab tab_home = new Tab(HomeFragment.class, R.string.home, R.drawable.selector_icon_home);
+        Tab tab_hot = new Tab(HotFragment.class, R.string.hot, R.drawable.selector_icon_hot);
+        Tab tab_category = new Tab(CategoryFragment.class, R.string.catagory, R.drawable
+                .selector_icon_category);
+        Tab tab_shop = new Tab(ShopCartFragment.class, R.string.cart, R.drawable
+                .selector_icon_cart);
+        Tab tab_mine = new Tab(MineFragment.class, R.string.mine, R.drawable.selector_icon_mine);
+
+        mTabs.add(tab_home);
+        mTabs.add(tab_hot);
+        mTabs.add(tab_category);
+        mTabs.add(tab_shop);
+        mTabs.add(tab_mine);
+        Log.v("sam","test");
+        mInflater = LayoutInflater.from(this);
+        mTabhost = (FragmentTabHost) this.findViewById(android.R.id.tabhost);
+        mTabhost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        for (Tab tab : mTabs) {
+            TabHost.TabSpec tabSpec = mTabhost.newTabSpec(getString(tab.getTitle()));
+            tabSpec.setIndicator(buildIndicator(tab));
+            mTabhost.addTab(tabSpec, tab.getFragment(), null);
+        }
+
+        mTabhost.getTabWidget().setShowDividers(LinearLayout.SHOW_DIVIDER_NONE);
+        mTabhost.setCurrentTab(0);           //默認選中第0個
+    }
+
+    private View buildIndicator(Tab tab) {
+
+        View view = mInflater.inflate(R.layout.tab_indicator, null);
+        ImageView img = (ImageView) view.findViewById(R.id.icon_tab);
+        TextView text = (TextView) view.findViewById(R.id.txt_indicator);
+
+        img.setImageResource(tab.getIcon());
+        text.setText(tab.getTitle());
+
+        return view;
+    }
+
+//控制物理返回鍵
+// 用來計算返回鍵的點擊間隔時間
+    private long exitTime = 0;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (System.currentTimeMillis() - exitTime > 2000) {
+                //ToastUtils.showSafeToast(MainActivity.this, "再點一次退出輕鬆購");
+                Toast.makeText(this,"再點一次退出",Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
